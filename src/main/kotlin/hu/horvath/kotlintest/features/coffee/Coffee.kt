02be504f0c1957d749c7ba13d8c84bce190f2884
phorvath <peter.horvath@smartcharging.hu>
2023-10-03 15:13:13 +0200

    trying to solve repository

--- src/main/kotlin/hu/horvath/kotlintest/features/coffee/Coffee.kt
+++ src/main/kotlin/hu/horvath/kotlintest/features/coffee/Coffee.kt
@@ -1,20 +1,20 @@
 package hu.horvath.kotlintest.features.coffee
 
-import jakarta.persistence.*
-import lombok.NoArgsConstructor
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.Id
 import java.math.BigDecimal
 
 @Entity
-@NoArgsConstructor
-data class Coffee(
+open class Coffee(
     @Id
     @GeneratedValue
     var id: String?,
 
-    @Column(name = "name")
     var name: String?,
 
-    val price: BigDecimal) {
+    var price: BigDecimal?
+) {
     constructor() : this(null, null, BigDecimal.ZERO)
-    }
+}
 

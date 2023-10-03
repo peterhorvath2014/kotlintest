b7ea72f333f1790d27630e463b034e67a98f9be3
phorvath <peter.horvath@smartcharging.hu>
2023-10-03 10:31:25 +0200

    rename project

--- src/main/kotlin/hu/horvath/kotlintest/KotlinTestApplication.kt
+++ src/main/kotlin/hu/horvath/kotlintest/KotlinTestApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class KotlintestApplication
+class KotlinTestApplication
 
 fun main(args: Array<String>) {
-	runApplication<KotlintestApplication>(*args)
+    runApplication<KotlinTestApplication>(*args)
 }
